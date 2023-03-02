825d6218a6415bedeb952b48c8a7ce9a81b31761
Nikolaos Mardas <112935888+IWantToLearn1996@users.noreply.github.com>
2023-03-02 18:54:40 +0100

    Add files via upload

---
+++ Day 17 - Brain Quiz - Copy/data.py
@@ -0,0 +1,15 @@
+question_data = [
+    {"text": "White is my favourite color.", "answer": "False"},
+    {"text": "Real Madrid is not my favorite team.", "answer": "False"},
+    {"text": "Fernando Alonso is my favorite F1 driver.", "answer": "False"},
+    {"text": "Boston Celtics is my favorite NBA team.", "answer": "False"},
+    {"text": "The car that I want is Nissan 350Z Nismo.", "answer": "True"},
+    {"text": "I live in Sweden.", "answer": "True"},
+    {"text": "VOLVO is my favorite company.", "answer": "True"},
+    {"text": "Los Angeles Lakers is my favorite NBA team.", "answer": "True"},
+    {"text": "I prefer Drift more than Drag races.", "answer": "True"},
+    {"text": "Michael Jordan is my favorite NBA player of all time.", "answer": "False"},
+    {"text": "I prefer Pizza over Gyros.", "answer": "False"},
+    {"text": "House is my favorite music", "answer": "True"}
+]
+
